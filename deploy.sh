15e4d1919c4d310c16cf40c2b9b15609d9857608
Antoine Taillefer <ataillefer@nuxeo.com>
2013-10-07 18:47:25 +0200

    Better quit nautilus in deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-killall -9 nautilus
+nautilus -q
 cp nautilus/NuxeoDriveFileInfoUpdater.py ~/.local/share/nautilus-python/extensions/.
 cp nautilus/icons/hicolor/48x48/emblems/* ~/.icons/hicolor/48x48/emblems/.
 nautilus&
