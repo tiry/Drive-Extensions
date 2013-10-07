c2bc8d8d277cd45d60ddfd5f6761009f8cdbd46c
Antoine Taillefer <ataillefer@nuxeo.com>
2013-10-07 16:38:59 +0200

    Deploy script: run nautilus in background

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 killall -9 nautilus
 cp nautilus/NuxeoDriveFileInfoUpdater.py ~/.local/share/nautilus-python/extensions/.
 cp nautilus/icons/hicolor/48x48/emblems/* ~/.icons/hicolor/48x48/emblems/.
-nautilus
+nautilus&
